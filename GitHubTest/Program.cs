5c69cb6c1d1e30546c96b2f27b6fe58afef6b4e4
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2024-09-23 12:37:30 +0200

    tilføjet hhbg

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Blib blob.");
+Console.WriteLine("Tilføjelse");
+Console.WriteLine("Blib blob.");
+Console.WriteLine("hello");
